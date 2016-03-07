1c01f433f09c0daf11895dbc6204fa5742ef2386
Wellington Silvestre <wellingtonsroberto@gmail.com>
2016-03-07 00:01:40 -0300

    Added files via upload

---
+++ Lampada.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOLab
+{
+   public class Lampada
+    {
+
+         private String modelo;
+         private int potencia;
+         private int voltagem;
+         private String localizacao;
+         private bool ligado;
+    
+        public void Instalar(String novaLoc)
+         {
+
+             localizacao = novaLoc;
+             Console.WriteLine("Instalado em" + localizacao);
+              
+         }
+         public void    Ligar()
+        {
+             if (!ligado)
+             {
+                 ligado = true;
+                 Console.WriteLine("L:" + localizacao + "ligada.");
+             }
+
+        }
+
+    public void Desligar()
+    {
+        if(ligado)
+   
+        ligado = false;
+       Console.WriteLine("L: {0} desligada.", localizacao);
+
+   }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOLab
+{
+    class Program
+    {
+        static void Main(string[] args)  // Main seguinifica "metodo" isso "string[] args" são parametros.
+        {
+
+            Lampada l = new Lampada();
+
+            l.Instalar("Sala");
+            l.Ligar();
+            l.Desligar();
+            l.Ligar();
+
+            
+
+        
+        }
+    }
+}
